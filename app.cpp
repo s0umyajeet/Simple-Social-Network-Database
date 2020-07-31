830b36abdd3e9a438e84ca5bee115ee06ed7c371
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-07-31 15:51:42 +0530

    [wip] | implementation for app.h

---
+++ app.cpp
@@ -0,0 +1,11 @@
+#include "app.h"
+using namespace std;
+
+void show_home_screen() {
+        cout << "---------------------------------------------------------------" << endl;
+        cout << "Choose an option: " << endl;
+        cout << "1. Add record" << endl;
+        cout << "2. Show complete member list" << endl;
+        cout << "3. Search for a member" << endl;
+        cout << "4. Access database" << endl;
+}
